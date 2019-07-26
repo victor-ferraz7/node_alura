ff91bb5b3d5595980166cf7054106540b0bb0c3c
Victor Ferraz <victor.fzn7@gmail.com>
2019-07-26 11:40:10 -0300

    criando rota para criar pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -2,5 +2,13 @@ module.exports = (app)=>{
     app.get('/pagamentos',(req,res)=> {
         res.send('Pagamentos :)');
         console.log('Requisição na rota /pagamentos');
-    })
+    });
+
+
+    app.post('/pagamentos/pagamento', (req,res)=>{
+    let pagamento = req.body;
+    console.log('Pagamento');
+    res.send('Pagamento Ok');
+});
+
 }
