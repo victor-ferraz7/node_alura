5a4b916eca79023f48db85df2e00ea18c213308d
Victor Ferraz <victor.fzn7@gmail.com>
2019-07-29 13:34:52 -0300

    Aplicando validações na entrada

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+
 
 module.exports = ()=>{
     let app = express();
-
+   
+    app.use(expressValidator());
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json());
+    
+    
 
     consign()
         .include('controllers')
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,7 +4,22 @@ module.exports = (app)=>{
         console.log('Requisição na rota /pagamentos');
     });
 
-    app.post('/pagamentos/pagamento', (req,res)=>{
+    app.post('/pagamentos/pagamento', (req,res)=> { 
+
+        req.assert("forma_de_pagamento","Forma de pagamento é obrigatório")
+                .notEmpty();
+        req.assert("valor", "valor é obrigatório e deve ser um decimal")
+                .notEmpty().isFloat();
+        
+        let erros = req.validationErrors();
+
+        if (erros){
+            console.log("Erros de validação encontrados");
+            res.status(400).send(erros);                           
+            return;
+        }
+
+
         let pagamento = req.body;
         console.log('Processando uma requisição de um novo pagamento');
     
@@ -16,7 +31,8 @@ module.exports = (app)=>{
 
         pagamentoDao.salva(pagamento, (erro, resultado) =>{
             if(erro){
-                res.send(erro);
+                console.log(`Erro ao inserir no banco ${erro}`);
+                res.status(400).send(erro);
             }
             else {
             console.log('Pagamento Criado');
