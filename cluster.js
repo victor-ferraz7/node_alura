85c76991868f0e86f1b413291c11671ee3389af4
Victor Ferraz <victor.fzn7@gmail.com>
2019-07-30 23:02:15 -0300

    Introdução Lib Cluster

---
+++ cluster.js
@@ -0,0 +1,11 @@
+const cluster = require('cluster');
+
+console.log('executando thread');
+
+if (cluster.isMaster){
+    console.log('thread master');
+    cluster.fork();
+}else{
+    console.log('thread slave');
+    cluster.fork();
+}
